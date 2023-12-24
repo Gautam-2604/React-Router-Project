d968f5099c36bed0dcc57a723cfabec8b67e9522
Gautam-2604 <gaurisariag@gmail.com>
2023-12-24 11:12:38 +0530

    final changes to header initially

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -35,7 +35,7 @@ export default function Header() {
                             <li>
                                 <NavLink
                                     className={({isActive}) =>
-                                        `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-red" : "text-grey"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                                        `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-orane-700" : "text-grey-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
                                     }
                                 >
                                     Home
